c689d6e4640a31f30849e97cc433b71d07b657b7
Raphael220904 <raphaelcosta213@yahoo.com.br>
2024-10-28 11:13:52 -0300

    Add files via upload

---
+++ DOM/script.js
@@ -0,0 +1,21 @@
+
+const meu_span = document.getElementById("container");
+
+let conteudo = ''; 
+
+const montaCard = (atleta) =>
+    <article>
+    <h1>${atleta.nome}</h1>
+    <img src=${atleta.imagem}>
+    <p>${atleta.detalhes}</p>
+    </article>
+
+/*for (let i = 0; i < dados.length; i++){
+    let atleta = dados[i];
+
+    conteudo += montaCard(atleta);
+}*/
+
+dados.forEach((atleta) => contudo += montaCard(atleta))
+
+container.innerHTML = conteudo;
