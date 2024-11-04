9dcf9aaf6f1c2543e83c0854c4971af2ea3d7eec
Raphael220904 <raphaelcosta213@yahoo.com.br>
2024-11-04 08:31:28 -0300

    Update script.js

--- DOM/script.js
+++ DOM/script.js
@@ -3,12 +3,13 @@ const meu_span = document.getElementById("container");
 
 let conteudo = ''; 
 
-const montaCard = (atleta) =>
+const montaCard = (atleta) => `
     <article>
-    <h1>${atleta.nome}</h1>
-    <img src=${atleta.imagem}>
-    <p>${atleta.detalhes}</p>
+        <h1>${atleta.nome}</h1>
+        <img src="${atleta.imagem}">
+        <p>${atleta.detalhes}</p>
     </article>
+`;
 
 /*for (let i = 0; i < dados.length; i++){
     let atleta = dados[i];
